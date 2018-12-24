1e06731204d05ae5b0f4306ac5f23d6db4be9751
Conte <boxiaozhi.bolu@gmail.com>
2018-12-25 00:18:20 +0800

    pure css, use buulma css“

--- resources/views/frontend/home/index.blade.php
+++ resources/views/frontend/home/index.blade.php
@@ -1,89 +1,73 @@
 @extends('layouts.main')
 @section('css')
     <style type="text/css">
-        body{
-            background-color: rgb(255, 255, 255);
-        }
-        .container{
-            padding: 10% 0;
-        }
-        .nickname{
-            font-size: 35px;
-            font-weight: 700;
-        }
-        .nickname>a:hover{
-            color: black;
-        }
-        .slogan{
-            padding: 5px;
-            font-weight: 700;
-        }
-        .contact{
-            margin-bottom: 10px;
-        }
-        .contact-body>a+a:before{
-            padding: 0 5px;
-            color: #000000;
-            content: "/\00a0";
-        }
-        .link-body,.contact-body{
-            padding: 8px 15px;
-            background-color: rgba(33, 33, 33, 0.15);
-        }
-        .link-body>a:hover{
-            color: #ffffff;
-        }
-        a {
-            color: #000000;
-        }
-        a:hover{
-            color: #ffffff;
+        .hero-footer{
+            padding: 1rem;
         }
     </style>
 @endsection
 @section('content')
-    <div class="container">
-        <div class="nickname text-center">
-            <a href="/">
-                <b>{{ $info['Nickname']['children'][0]['text'] }}</b>
-            </a>
-        </div>
-        <div class="slogan text-center">
-            @php
-                $index = rand(0,2);
-                $slogan = $info['Slogan']['children'][$index]['text'];
-            @endphp
-            <p class="">{{ $slogan }}</p>
-        </div>
-        <div class="contact text-center">
-            <div class="contact-body">
-                @foreach($info['Contact']['children'] as $item)
+    <section class="hero is-dark is-fullheight">
+        <div class="hero-body">
+            <div class="container">
+                <h1 class="title">
+                    <a href="/">
+                        <b>{{ $info['Nickname']['children'][0]['text'] }}</b>
+                    </a>
+                </h1>
+                <h2 class="subtitle">
                     @php
-                        $url = getUrl($item['note']);
+                        $index = rand(0,2);
+                        $slogan = $info['Slogan']['children'][$index]['text'];
                     @endphp
-                    @if($url)
-                        <a class="margin-r-5" href="{{ $url }}" target="_self" title="{{ $url }}">{{ $item['text'] }}</a>
-                    @else
-                        <a class="margin-r-5" title="{{ $item['note'] }}">{{ $item['text'] }}</a>
-                    @endif
-                @endforeach
+                    <p class="">{{ $slogan }}</p>
+                </h2>
+                <nav class="breadcrumb is-small" aria-label="breadcrumbs">
+                    <ul>
+                        @foreach($info['Contact']['children'] as $item)
+                            <li>
+                            @php
+                                    $url = getUrl($item['note']);
+                                @endphp
+                                @if($url)
+                                        <a href="{{ $url }}" target="_self" title="{{ $url }}">{{ $item['text'] }}</a>
+
+                                @else
+                                        <a title="{{ $item['note'] }}">{{ $item['text'] }}</a>
+                                @endif
+                            </li>
+                        @endforeach
+                    </ul>
+                </nav>
+                <nav class="breadcrumb is-small" aria-label="breadcrumbs">
+                    <ul>
+                        @foreach($info['SiteLink']['children'] as $item)
+                            <li>
+                            @php
+                                $url = getUrl($item['note']);
+                            @endphp
+                            @if($url)
+                                <a href="{{ $url }}" target="_self" title="{{ $url }}">{{ $item['text'] }}</a>
+                            @else
+                                <a title="{{ $item['note'] }}">{{ $item['text'] }}</a>
+                            @endif
+                            </li>
+                        @endforeach
+                    </ul>
+                </nav>
             </div>
         </div>
-        <div class="link text-center">
-            <div class="link-body">
-                @foreach($info['SiteLink']['children'] as $item)
-                    @php
-                        $url = getUrl($item['note']);
-                    @endphp
-                    @if($url)
-                        <a class="margin-r-5" href="{{ $url }}" target="_self" title="{{ $url }}">{{ $item['text'] }}</a>
-                    @else
-                        <a class="margin-r-5" title="{{ $item['note'] }}">{{ $item['text'] }}</a>
-                    @endif
-                @endforeach
-            </div>
+        <div class="hero-footer">
+            <nav class="breadcrumb is-small is-centered" aria-label="breadcrumbs">
+                <ul>
+                    <li><a>© {{ date('Y') }}</a></li>
+                    <li><a href="https://github.com/boxiaozhi/isconte"><b>isconte</b></a></li>
+                    <li><a href="https://github.com/laravel/laravel/">Powered by<strong>&nbsp;Laravel</strong></a></li>
+                    <li><a>粤ICP备17015159号</a></li>
+                </ul>
+            </nav>
         </div>
-    </div>
+    </section>
 @endsection
 
 @section('script')
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,29 +1,12 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-108159539-1"></script>
-<script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', 'UA-108159539-1');
-</script>
-<!-- baidu -->
-<script>
-    var _hmt = _hmt || [];
-    (function() {
-        var hm = document.createElement("script");
-        hm.src = "https://hm.baidu.com/hm.js?57b9ef85fb5c8dc96b8d3e0cdd746545";
-        var s = document.getElementsByTagName("script")[0];
-        s.parentNode.insertBefore(hm, s);
-    })();
-</script>
+
 @include('layouts.header')
 <body>
-    <div class="content">
-        @yield('content')
-        @include('layouts.script')
-    </div>
-    @include('layouts.footer')
+    @yield('content')
+    @include('layouts.script')
+    @if(!in_array($viewName, ['frontend.home.index']))
+        @include('layouts.footer')
+    @endif
 </body>
 </html>
