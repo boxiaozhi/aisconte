d6ba8d4a18d5c06ff503aa07ceed4b2ffb03fe64
Conte <boxiaozhi.bolu@gmail.com>
2019-11-24 13:45:06 +0800

    about beian

--- resources/views/frontend/layouts/_footer.blade.php
+++ resources/views/frontend/layouts/_footer.blade.php
@@ -1,7 +1,7 @@
 <nav class="breadcrumb is-small is-centered" aria-label="breadcrumbs">
     <ul>
         <li><a>© {{ date('Y') }}</a></li>
-        <li><a href="https://github.com/boxiaozhi/isconte"><b>isconte</b></a></li>
-        <li><a>粤ICP备17015159号</a></li>
+        <li><a href=""><b>isconte</b></a></li>
+        <li><a href="http://www.beian.miit.gov.cn/">粤ICP备17015159号-1</a></li>
     </ul>
 </nav>
